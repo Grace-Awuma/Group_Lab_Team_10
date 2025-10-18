728c2435b58439a438c389b83c8e69e1d61372e0
Yaksha15 <poladia.y@northeastern.edu>
2025-10-18 12:31:40 -0400

    demo file

---
+++ Member2/src/member2/Member2.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package member2;
+
+/**
+ *
+ * @author Yaksha
+ */
+public class Member2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello from Member 2 Yaksha!");
+
+    }
+    
+}
