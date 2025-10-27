47d945aed077b74ecd413027c1bf2e6c3fb57299
dmehta1310 <mehta.dh@northeastern.edu>
2025-10-26 23:33:05 -0400

    department class updated

--- University_model_Lab/src/Business/Department/DepartmentDirectory.java
+++ University_model_Lab/src/Business/Department/DepartmentDirectory.java
@@ -4,10 +4,35 @@
  */
 package Business.Department;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author Yaksha
  */
 public class DepartmentDirectory {
-    
+    ArrayList<Department> departmentlist;
+
+    public DepartmentDirectory() {
+        departmentlist = new ArrayList<>();
+    }
+
+    public Department newDepartment(String id, String name) {
+        Department dept = new Department(id, name);
+        departmentlist.add(dept);
+        return dept;
+    }
+
+    public Department findDepartment(String id) {
+        for (Department dept : departmentlist) {
+            if (dept.getDepartmentId().equals(id)) {
+                return dept;
+            }
+        }
+        return null;
+    }
+
+    public ArrayList<Department> getDepartmentList() {
+        return departmentlist;
+    }
 }
