051788ab45d1ace5036123316045b3dfdfce6a2b
dmehta1310 <mehta.dh@northeastern.edu>
2025-10-26 23:35:18 -0400

    person class updated

--- University_model_Lab/src/Business/Person/Person.java
+++ University_model_Lab/src/Business/Person/Person.java
@@ -4,10 +4,79 @@
  */
 package Business.Person;
 
+
 /**
  *
  * @author Yaksha
  */
 public class Person {
-    
+    String id;
+    String firstName;
+    String lastName;
+    String email;
+    String phone;
+    String address;
+
+    public Person(String id, String firstName, String lastName) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public String getPersonId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getFullName() {
+        return firstName + " " + lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public boolean isMatch(String id) {
+        return getPersonId().equals(id);
+    }
+
+    @Override
+    public String toString() {
+        return getFullName();
+    }
 }
