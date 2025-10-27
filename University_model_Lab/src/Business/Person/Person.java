bba9ada66ba91515d7b712bf22ccc522a3aa165c
Grace-Awuma <awuma.g@northeastern.edu>
2025-10-27 01:58:53 -0400

    Person and Person Directory linked

--- University_model_Lab/src/Business/Person/Person.java
+++ University_model_Lab/src/Business/Person/Person.java
@@ -2,12 +2,113 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
+ 
 package Business.Person;
 
-/**
- *
- * @author Yaksha
- */
 public class Person {
-    
+   String id;
+   String firstName;
+   String lastName;
+   String email;
+   String phone;
+   String address;
+   String city;
+   String state;
+   String zipCode;
+
+   public Person(String id, String firstName, String lastName) {
+      this.id = id;
+      this.firstName = firstName;
+      this.lastName = lastName;
+   }
+
+   public String getPersonId() {
+      return this.id;
+   }
+
+   public String getFirstName() {
+      return this.firstName;
+   }
+
+   public void setFirstName(String firstName) {
+      this.firstName = firstName;
+   }
+
+   public String getLastName() {
+      return this.lastName;
+   }
+
+   public void setLastName(String lastName) {
+      this.lastName = lastName;
+   }
+
+   public String getFullName() {
+      return this.firstName + " " + this.lastName;
+   }
+
+   public String getEmail() {
+      return this.email;
+   }
+
+   public void setEmail(String email) {
+      this.email = email;
+   }
+
+   public String getPhone() {
+      return this.phone;
+   }
+
+   public void setPhone(String phone) {
+      this.phone = phone;
+   }
+
+   public String getAddress() {
+      return this.address;
+   }
+
+   public void setAddress(String address) {
+      this.address = address;
+   }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getZipCode() {
+        return zipCode;
+    }
+
+    public void setZipCode(String zipCode) {
+        this.zipCode = zipCode;
+    }
+   
+   
+
+   public boolean isMatch(String id) {
+      return this.getPersonId().equals(id);
+   }
+
+   public String toString() {
+      return this.getFullName();
+   }
 }
--- University_model_Lab/src/Business/Person/PersonDirectory.java
+++ University_model_Lab/src/Business/Person/PersonDirectory.java
@@ -2,12 +2,65 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
+ 
 package Business.Person;
 
-/**
- *
- * @author Yaksha
- */
+import java.util.ArrayList;
+import java.util.Iterator;
+
 public class PersonDirectory {
-    
+   ArrayList<Person> personlist = new ArrayList();
+   private int personCounter = 1001;
+
+   public Person newPerson(String firstName, String lastName) {
+      Object[] var10001 = new Object[]{this.personCounter++};
+      String id = "PER" + String.format("%06d", var10001);
+      Person p = new Person(id, firstName, lastName);
+      this.personlist.add(p);
+      return p;
+   }
+
+   public Person newPersonWithId(String id, String firstName, String lastName) {
+      Person p = new Person(id, firstName, lastName);
+      this.personlist.add(p);
+      return p;
+   }
+
+   public Person findPerson(String id) {
+      Iterator var2 = this.personlist.iterator();
+
+      Person p;
+      do {
+         if (!var2.hasNext()) {
+            return null;
+         }
+
+         p = (Person)var2.next();
+      } while(!p.isMatch(id));
+
+      return p;
+   }
+
+   public boolean emailExists(String email) {
+      Iterator var2 = this.personlist.iterator();
+
+      Person p;
+      do {
+         if (!var2.hasNext()) {
+            return false;
+         }
+
+         p = (Person)var2.next();
+      } while(p.getEmail() == null || !p.getEmail().equalsIgnoreCase(email));
+
+      return true;
+   }
+
+   public ArrayList<Person> getPersonList() {
+      return this.personlist;
+   }
+
+   public String generateId(String prefix) {
+      return prefix + String.format("%06d", this.personCounter++);
+   }
 }
