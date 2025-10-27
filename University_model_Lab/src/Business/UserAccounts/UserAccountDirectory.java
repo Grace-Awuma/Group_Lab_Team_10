c5c888ced76f33b96966680ae9440678e0486d4b
Yaksha15 <poladia.y@northeastern.edu>
2025-10-26 23:26:43 -0400

    user accounts updated

--- University_model_Lab/src/Business/UserAccounts/UserAccountDirectory.java
+++ University_model_Lab/src/Business/UserAccounts/UserAccountDirectory.java
@@ -4,10 +4,59 @@
  */
 package Business.UserAccounts;
 
+import Business.Profiles.Profile;
+import java.util.ArrayList;
+
 /**
  *
  * @author Yaksha
  */
 public class UserAccountDirectory {
     
+    private ArrayList<UserAccount> useraccountlist;
+    
+    public UserAccountDirectory() {
+        useraccountlist = new ArrayList<>();
+    }
+    
+    public UserAccount newUserAccount(Profile role, String username, String password) {
+        UserAccount ua = new UserAccount(role, username, password);
+        useraccountlist.add(ua);
+        return ua;
+    }
+    
+    public UserAccount authenticateUser(String username, String password) {
+        for (UserAccount ua : useraccountlist) {
+            if (ua.isValidUser(username, password)) {
+                return ua;
+            }
+        }
+        return null;
+    }
+    
+    public UserAccount findUserAccountByPersonId(String personId) {
+        for (UserAccount ua : useraccountlist) {
+            if (ua.isMatch(personId)) {
+                return ua;
+            }
+        }
+        return null;
+    }
+    
+    public UserAccount findUserAccountByUsername(String username) {
+        for (UserAccount ua : useraccountlist) {
+            if (ua.getUserLoginName().equalsIgnoreCase(username)) {
+                return ua;
+            }
+        }
+        return null;
+    }
+    
+    public boolean usernameExists(String username) {
+        return findUserAccountByUsername(username) != null;
+    }
+    
+    public ArrayList<UserAccount> getUserAccountList() {
+        return useraccountlist;
+    }
 }
