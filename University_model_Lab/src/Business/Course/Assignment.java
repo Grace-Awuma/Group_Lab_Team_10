363cc00fb185c0aae226691d6925a631dd4ca075
rithwikgokeneu <goke.r@northeastern.edu>
2025-10-26 23:05:59 -0400

    Course Class Updated

--- University_model_Lab/src/Business/Course/Assignment.java
+++ University_model_Lab/src/Business/Course/Assignment.java
@@ -9,5 +9,62 @@ package Business.Course;
  * @author Yaksha
  */
 public class Assignment {
-    
+    String assignmentId;
+    String title;
+    String description;
+    double weight;
+    Double score;
+    String submissionStatus;
+
+    public Assignment(String assignmentId, String title, String description, double weight) {
+        this.assignmentId = assignmentId;
+        this.title = title;
+        this.description = description;
+        this.weight = weight;
+        this.submissionStatus = "Not Submitted";
+    }
+
+    public String getAssignmentId() {
+        return assignmentId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    public Double getScore() {
+        return score;
+    }
+
+    public void setScore(Double score) {
+        this.score = score;
+    }
+
+    public String getSubmissionStatus() {
+        return submissionStatus;
+    }
+
+    public void setSubmissionStatus(String submissionStatus) {
+        this.submissionStatus = submissionStatus;
+    }
 }
