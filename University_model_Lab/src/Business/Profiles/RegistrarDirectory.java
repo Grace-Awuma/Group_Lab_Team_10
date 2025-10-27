771ee3c6100619b341de46d822d9cee8b4a86deb
Yaksha15 <poladia.y@northeastern.edu>
2025-10-26 23:24:22 -0400

    registrar profile updated

--- University_model_Lab/src/Business/Profiles/RegistrarDirectory.java
+++ University_model_Lab/src/Business/Profiles/RegistrarDirectory.java
@@ -4,10 +4,39 @@
  */
 package Business.Profiles;
 
+import Business.Business;
+import Business.Person.Person;
+import java.util.ArrayList;
+
 /**
  *
  * @author Yaksha
  */
 public class RegistrarDirectory {
-    
+    Business business;
+    ArrayList<RegistrarProfile> registrarlist;
+
+    public RegistrarDirectory(Business b) {
+        business = b;
+        registrarlist = new ArrayList<>();
+    }
+
+    public RegistrarProfile newRegistrarProfile(Person p) {
+        RegistrarProfile rp = new RegistrarProfile(p);
+        registrarlist.add(rp);
+        return rp;
+    }
+
+    public RegistrarProfile findRegistrar(String id) {
+        for (RegistrarProfile rp : registrarlist) {
+            if (rp.isMatch(id)) {
+                return rp;
+            }
+        }
+        return null;
+    }
+
+    public ArrayList<RegistrarProfile> getRegistrarList() {
+        return registrarlist;
+    }
 }
