546b7fe37972106b01a7948acf67f9e2996fe061
rithwikgokeneu <goke.r@northeastern.edu>
2025-10-26 22:57:52 -0400

    Course java class

--- University_model_Lab/src/Business/Course/Course.java
+++ University_model_Lab/src/Business/Course/Course.java
@@ -9,5 +9,62 @@ package Business.Course;
  * @author Yaksha
  */
 public class Course {
-    
+    String courseId;
+    String courseName;
+    String description;
+    int creditHours;
+    String department;
+
+    public Course(String courseId, String courseName, String description, int creditHours, String department) {
+        this.courseId = courseId;
+        this.courseName = courseName;
+        this.description = description;
+        this.creditHours = creditHours;
+        this.department = department;
+    }
+
+    public String getCourseId() {
+        return courseId;
+    }
+
+    public void setCourseId(String courseId) {
+        this.courseId = courseId;
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getCreditHours() {
+        return creditHours;
+    }
+
+    public void setCreditHours(int creditHours) {
+        this.creditHours = creditHours;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    @Override
+    public String toString() {
+        return courseId + " - " + courseName;
+    }
 }
